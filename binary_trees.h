bb2f6a76010df4e33ecb5ab0aa33f825490ca036
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-09-01 00:47:08 -0700

    inc

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,9 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -21,6 +23,7 @@ struct binary_tree_s
 } binary_tree_t;
 
 typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -40,4 +43,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+
 #endif
